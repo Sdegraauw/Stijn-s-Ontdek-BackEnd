c2572b0d8bbf452e72040960846aaaf825467309
Cyrion <cyrion.vandongen@student.fontys.nl>
2022-03-15 10:27:45 +0100

    Setup base project

---
+++ ClimateChecker/src/test/java/Ontdekstation013/ClimateChecker/ClimateCheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package Ontdekstation013.ClimateChecker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClimateCheckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
