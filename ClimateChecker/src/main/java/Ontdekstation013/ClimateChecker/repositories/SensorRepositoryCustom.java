4e797d4e48c392dcc17f2e20939215bf23c8e5fe
Janno <jannofox01@gmail.com>
2022-04-28 15:05:55 +0200

    Add custom query

--- ClimateChecker/src/main/java/Ontdekstation013/ClimateChecker/repositories/SensorRepositoryCustom.java
+++ ClimateChecker/src/main/java/Ontdekstation013/ClimateChecker/repositories/SensorRepositoryCustom.java
@@ -1,12 +1,15 @@
 package Ontdekstation013.ClimateChecker.repositories;
 
 import Ontdekstation013.ClimateChecker.models.Sensor;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface SensorRepositoryCustom {
 
+    @Query(value = "SELECT * FROM SENSOR WHERE SENSOR_TYPE = ?1", nativeQuery = true)
     List<Sensor> findAllByType(long typeId);
 
+    @Query(value = "SELECT * FROM SENSOR WHERE STATIONID = ?1", nativeQuery = true)
     List<Sensor> findAllByStation(long stationId);
 }
