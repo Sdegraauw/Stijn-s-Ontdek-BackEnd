d6b217a60a5ce5ac6284972e3a5f7ae80750461b
Quinn Richardson <quinnmykah@gmail.com>
2022-12-01 10:29:07 +0100

    Changes in login and email services

--- ClimateChecker/src/main/java/Ontdekstation013/ClimateChecker/services/EmailSenderService.java
+++ ClimateChecker/src/main/java/Ontdekstation013/ClimateChecker/services/EmailSenderService.java
@@ -30,7 +30,7 @@ public class EmailSenderService {
         System.out.printf("Mail Send");
     }
 
-    public void sendSignupMail(String toEmail, String firstName, String lastName, String linkHash) throws MessagingException {
+    public void sendSignupMail(String toEmail, String firstName, String lastName, String link) throws MessagingException {
         MimeMessage message = mailSender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(message, true);
 
@@ -41,7 +41,7 @@ public class EmailSenderService {
                 + "<br>"
                 + "<br>"
                 + "<br>"
-                + linkHash
+                + link
                 + "<br>"
                 + "<br>"
                 + "Met vriendelijke groet,"
@@ -60,7 +60,7 @@ public class EmailSenderService {
         System.out.printf("Mail Sent");
     }
 
-    public void sendLoginMail(String toEmail, String firstName, String lastName, String linkHash) throws MessagingException {
+    public void sendLoginMail(String toEmail, String firstName, String lastName, String link) throws MessagingException {
         MimeMessage message = mailSender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(message, true);
 
@@ -70,7 +70,49 @@ public class EmailSenderService {
         String body = "Inlog link: ,"
                 + "<br>"
                 + "<br> [insert beautiful html button]"
-                + linkHash
+                + link
+                + "<br>"
+                + "<br>"
+                + "Met vriendelijke groet,"
+                + "<br>"
+                + " Ontdekstation 013"
+                + "<br>"
+                + "<img src=\"cid:logo.png\"></img><br/>";
+
+        helper.setTo("kayletmail@host.com");
+        helper.setTo(toEmail);
+        helper.setSubject(String.format("Welkom %s", firstName + " " + lastName));
+        helper.setText(body, true);
+
+        mailSender.send(message);
+
+        System.out.printf("Mail Sent");
+    }
+
+    public void deleteUserMail(String toEmail, String firstName, String lastName) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        String body = "we're extremely utterly insanely incredibly sorry to see you go :( sadface";
+        message.setFrom("kayletmail@gmail.com");
+        message.setTo(toEmail);
+        message.setSubject(String.format("Welcome %s", firstName + " " + lastName));
+        message.setText(String.format("Welcome %s", body));
+
+
+        mailSender.send(message);
+    }
+
+    public void sendEmailEditMail(String toEmail, String firstName, String lastName, String link) throws MessagingException {
+        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+
+//        File file = new File(".\\ClimateChecker\\src\\main\\resources\\images\\ontdekstation013-logo.png");
+//        helper.addAttachment("logo.png", file);
+
+        String body = "Je hebt net je email changed 013,"
+                + "<br>"
+                + "<br>"
+                + "<br>"
+                + link
                 + "<br>"
                 + "<br>"
                 + "Met vriendelijke groet,"
