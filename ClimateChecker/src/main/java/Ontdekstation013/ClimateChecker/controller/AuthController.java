7fab8b7dc681e689738b172f1ed5c9b93eea451c
Quinn Richardson <quinnmykah@gmail.com>
2022-12-06 01:16:21 +0100

    Added exceptions for userservice

--- ClimateChecker/src/main/java/Ontdekstation013/ClimateChecker/controller/AuthController.java
+++ ClimateChecker/src/main/java/Ontdekstation013/ClimateChecker/controller/AuthController.java
@@ -19,47 +19,33 @@ import org.springframework.web.bind.annotation.*;
 
 public class AuthController {
     private final UserService userService;
-    private final EmailSenderService emailSenderService;
 
     @Autowired
-    public AuthController(UserService userService, EmailSenderService emailSenderService)
+    public AuthController(UserService userService)
     {
         this.userService = userService;
-        this.emailSenderService = emailSenderService;
     }
 
     // create new user
     @PostMapping("register")
     public ResponseEntity<userDto> createNewUser(@RequestBody registerDto registerDto) throws Exception {
         User user = userService.createNewUser(registerDto);
-        if (user != null) {
-            Token token = userService.createToken(user);
-            userService.saveToken(token);
-            emailSenderService.sendSignupMail(user.getMailAddress(), user.getFirstName(), user.getLastName(), userService.createLink(token));
-            return ResponseEntity.status(HttpStatus.CREATED).body(null);
-        }
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        return ResponseEntity.status(HttpStatus.CREATED).body(null);
     }
 
     // login user
     @PostMapping("login")
     public ResponseEntity<userDto> loginUser(@RequestBody loginDto loginDto) throws Exception {
-        User user = userService.verifyMail(loginDto);
-        if (user != null){
-            Token token = userService.createToken(user);
-            userService.saveToken(token);
-            emailSenderService.sendLoginMail(user.getMailAddress(), user.getFirstName(), user.getLastName(), userService.createLink(token));
-            return ResponseEntity.status(HttpStatus.OK).body(null);
-        }
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        userService.verifyMail(loginDto);
+        return ResponseEntity.status(HttpStatus.OK).body(null);
     }
 
-    @GetMapping("/verify")
+    // verify the login / (or first register)
+    @GetMapping("verify")
     public ResponseEntity<userDto> fetchLink(@RequestParam String linkHash, @RequestParam String email){
         if (userService.verifyToken(linkHash, email)){
             return ResponseEntity.status(HttpStatus.OK).body(null);
         }
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
     }
-
 }
--- ClimateChecker/src/main/java/Ontdekstation013/ClimateChecker/services/UserService.java
+++ ClimateChecker/src/main/java/Ontdekstation013/ClimateChecker/services/UserService.java
@@ -1,18 +1,17 @@
 package Ontdekstation013.ClimateChecker.services;
+import Ontdekstation013.ClimateChecker.exception.BadRequestException;
+import Ontdekstation013.ClimateChecker.exception.ConflictException;
 import Ontdekstation013.ClimateChecker.models.Token;
 import Ontdekstation013.ClimateChecker.models.User;
 import Ontdekstation013.ClimateChecker.models.dto.*;
 import Ontdekstation013.ClimateChecker.repositories.TokenRepository;
 import Ontdekstation013.ClimateChecker.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
-import org.springframework.web.server.handler.ResponseStatusExceptionHandler;
 
+import javax.mail.SendFailedException;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -22,13 +21,14 @@ public class UserService {
 
     private final UserRepository userRepository;
     private final TokenRepository tokenRepository;
-    private final EmailSenderService emailService;
+    private final EmailSenderService emailSenderService;
+    private PasswordEncoder encoder = new BCryptPasswordEncoder();
 
     @Autowired
-    public UserService(UserRepository userRepository, TokenRepository tokenRepository, EmailSenderService emailService) {
+    public UserService(UserRepository userRepository, TokenRepository tokenRepository, EmailSenderService emailSenderService) {
         this.userRepository = userRepository;
         this.tokenRepository = tokenRepository;
-        this.emailService = emailService;
+        this.emailSenderService = emailSenderService;
     }
 
     public userDto findUserById(long id) {
@@ -78,58 +78,52 @@ public class UserService {
     }
 
     public userDto editUser(editUserDto editUserDto) throws Exception {
-        List<String> errorList = new ArrayList<>();
         User user = userRepository.findById(editUserDto.getId()).orElseThrow();
 
         if (editUserDto.getFirstName().length() < 256)
             user.setFirstName(editUserDto.getFirstName());
         else
-            errorList.add("First name can't be longer than 256 characters");
+            throw new BadRequestException(null); //First name can't be longer than 256 characters
 
         if (editUserDto.getLastName().length() < 256)
             user.setLastName(editUserDto.getLastName());
         else
-            errorList.add("Last name can't be longer than 256 characters");
+            throw new BadRequestException(null); //Last name can't be longer than 256 characters
+
 
         if (editUserDto.getUserName().length() < 256) {
             if (!editUserDto.getUserName().equals(user.getUserName())) {
                 if (!userRepository.existsUserByUserName(editUserDto.getUserName())) {
                     user.setUserName(editUserDto.getUserName());
                 } else {
-                    errorList.add("Username already in use");
+                    throw new ConflictException(1); //Username previously used
                 }
             }
         } else {
-            errorList.add("Last name can't be longer than 256 characters");
+            throw new BadRequestException(null); //Last name can't be longer than 256 characters
         }
 
         editUserDto.setMailAddress(editUserDto.getMailAddress().toLowerCase());
         if (!user.getMailAddress().equals(editUserDto.getMailAddress())) {
             if (editUserDto.getMailAddress().contains("@")) {
                 if (!userRepository.existsUserByMailAddress(editUserDto.getMailAddress())) {
-                    if (errorList.size() == 0) {
-                        String mail = user.getMailAddress();
-                        user.setMailAddress(editUserDto.getMailAddress());
-                        Token token = createToken(user);
-                        user.setMailAddress(mail);
-                        token.setUser(user);
-                        saveToken(token);
-                        emailService.sendEmailEditMail(editUserDto.getMailAddress(), user.getFirstName(), user.getLastName(), createLink(token, editUserDto.getMailAddress()));
+                    String mail = user.getMailAddress();
+                    user.setMailAddress(editUserDto.getMailAddress());
+                    Token token = createToken(user);
+                    user.setMailAddress(mail);
+                    token.setUser(user);
+                    saveToken(token);
+                    try {
+                        emailSenderService.sendEmailEditMail(editUserDto.getMailAddress(), user.getFirstName(), user.getLastName(), createLink(token, editUserDto.getMailAddress()));
+                    } catch(SendFailedException e) {
+                        throw new BadRequestException(null); //email failed to send
                     }
                 } else {
-                    errorList.add("Email address already in use");
+                    throw new ConflictException(2); //Email already previously used
                 }
             } else {
-                errorList.add("Invalid email address");
+                throw new BadRequestException(null); //Invalid email address
             }
-        }
-
-        if (errorList.size() != 0){
-            StringBuilder errorResponse = new StringBuilder();
-            for(String error : errorList){
-                errorResponse.append(error).append("\n");
-            }
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, errorResponse.toString());
         }
 
         return userToUserDto(userRepository.save(user));
@@ -137,29 +131,70 @@ public class UserService {
 
     public void deleteUser(long id) {
         User user = userRepository.getById(id);
-        emailService.deleteUserMail(user.getMailAddress(), user.getFirstName(), user.getLastName());
+        emailSenderService.deleteUserMail(user.getMailAddress(), user.getFirstName(), user.getLastName());
         userRepository.deleteById(id);
     }
 
-    public User createNewUser(registerDto registerDto) {
-        if (registerDto.getFirstName().length() < 256 && registerDto.getLastName().length() < 256 && registerDto.getUserName().length() < 256) {
-            registerDto.setMailAddress(registerDto.getMailAddress().toLowerCase());
-            if (registerDto.getMailAddress().contains("@")) {
-                if (!userRepository.existsUserByUserNameOrMailAddress(registerDto.getUserName(), registerDto.getMailAddress())) {
-                    User user = new User(registerDto.getMailAddress(), registerDto.getFirstName(), registerDto.getLastName(), registerDto.getUserName());
-                    return userRepository.save(user);
-                }
+    public User createNewUser(registerDto registerDto) throws Exception {
+        User user = new User();
+        if (registerDto.getFirstName().length() > 256) {
+            throw new BadRequestException(null); //First name can't be longer than 256 characters
+        }
+        if (registerDto.getLastName().length() > 256) {
+            throw new BadRequestException(null); //Last name can't be longer than 256 characters
+        }
+
+        if (registerDto.getUserName().length() < 256) {
+            if (!userRepository.existsUserByUserName(registerDto.getUserName())) {
+                user.setUserName(registerDto.getUserName());
+            } else {
+                throw new ConflictException(1); //Username previously used
+            }
+        } else {
+            throw new BadRequestException(null); //Last name can't be longer than 256 characters
+        }
+
+        registerDto.setMailAddress(registerDto.getMailAddress().toLowerCase());
+        if (registerDto.getMailAddress().contains("@")) {
+            if (!userRepository.existsUserByMailAddress(registerDto.getMailAddress())) {
+                user = new User(registerDto.getMailAddress(), registerDto.getFirstName(), registerDto.getLastName(), registerDto.getUserName());
+            } else {
+                throw new ConflictException(2); //Username previously used
+            }
+        } else {
+            throw new BadRequestException(null); //invalid email adress
+        }
+
+        user = userRepository.save(user);
+        Token token = createToken(user);
+        saveToken(token);
+
+        try {
+            emailSenderService.sendSignupMail(user.getMailAddress(), user.getFirstName(), user.getLastName(), createLink(token));
+        } catch(SendFailedException e) {
+            throw new BadRequestException(null); //email failed to send
+        }
+
+        return user;
+    }
+
+    public User verifyMail(loginDto loginDto) throws Exception {
+        User user = userRepository.findByMailAddress(loginDto.getMailAddress());
+        if (user == null){
+            throw new BadRequestException(null); //user not found
+        } else if (user != null){
+            Token token = createToken(user);
+            saveToken(token);
+            try {
+                emailSenderService.sendLoginMail(user.getMailAddress(), user.getFirstName(), user.getLastName(), createLink(token));
+            } catch(SendFailedException e) {
+                throw new BadRequestException(null); //email failed to send
             }
         }
-        return null;
-    }
-
-    public User verifyMail(loginDto loginDto) {
-        return userRepository.findByMailAddress(loginDto.getMailAddress());
+        return user;
     }
 
 
-    PasswordEncoder encoder = new BCryptPasswordEncoder();
 
     public Token createToken(User user) {
         Token token = new Token();
@@ -167,7 +202,6 @@ public class UserService {
         token.setUser(user);
         token.setCreationTime(LocalDateTime.now());
         token.setLinkHash(encoder.encode(user.getMailAddress() + user.getUserID()));
-
 
         return token;
     }
@@ -194,7 +228,8 @@ public class UserService {
         return false;
     }
 
-    public boolean verifyToken(String linkHash, String oldEmail, String newEmail) {
+
+    public boolean verifyToken(String linkHash, String oldEmail, String newEmail) { //for changing to new email address
         User user = userRepository.findByMailAddress(oldEmail);
         Token officialToken = tokenRepository.findByUser(user);
         if (officialToken != null){
@@ -215,7 +250,7 @@ public class UserService {
         return (domain + "api/Authentication/verify" + "?linkHash=" + token.getLinkHash() + "&email=" + token.getUser().getMailAddress());
     }
 
-    public String createLink(Token token, String newEmail){
+    public String createLink(Token token, String newEmail){ //for changing to new email address
         String domain = "http://localhost:8082/";
         return (domain + "api/User/verify" + "?linkHash=" + token.getLinkHash() + "&oldEmail=" + token.getUser().getMailAddress() + "&newEmail=" + newEmail);
     }
