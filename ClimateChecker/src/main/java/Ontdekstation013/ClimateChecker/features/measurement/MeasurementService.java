310a3b74bc56340fc5011519f3c946f977c47d84
Lars-v-Delft <lars171998@gmail.com>
2023-10-09 15:07:47 +0200

    Merge branch 'Feature/#69' of https://github.com/OntdekIT/Software-BackEnd into Feature/#69

--- ClimateChecker/src/main/java/Ontdekstation013/ClimateChecker/features/measurement/MeasurementService.java
+++ ClimateChecker/src/main/java/Ontdekstation013/ClimateChecker/features/measurement/MeasurementService.java
@@ -1,7 +1,12 @@
 package Ontdekstation013.ClimateChecker.features.measurement;
 
-import java.util.List;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.*;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
@@ -12,33 +17,60 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import static java.util.Comparator.comparingInt;
+import static java.util.stream.Collectors.collectingAndThen;
+import static java.util.stream.Collectors.toCollection;
+
 @Service
 public class MeasurementService {
 
     RestTemplate restTemplate = new RestTemplate();
     private Logger LOG = LoggerFactory.getLogger(MeasurementService.class);
-    private String BASE_URL = "https://meetjestad.net/data/?type=sensors&format=json&limit=100";
+    private String BASE_URL = "https://meetjestad.net/data/?type=sensors&format=json&limit=1000";
 
-    public List<Measurement> getFilterMeasurements(String startDate, String endDate) {
+    public List<Measurement> getLatestMeasurements(int minuteLimit) throws ParseException, JsonProcessingException {
+        Instant endMoment = Instant.now();
+        Instant startMoment = endMoment.minus(Duration.ofMinutes(minuteLimit));
 
-        try {
+        String startDateTime = meetJeStadDateTimeConverter(startMoment);
+        String endDateTime = meetJeStadDateTimeConverter(endMoment);
 
-            if (startDate != null && endDate != null) {
-                BASE_URL += "&start=" + startDate + "&end=" + endDate;
+        // get measurements from MeetJeStadAPI
+        List<Measurement> latestMeasurements = getMeetJeStadMeasurements(startDateTime, endDateTime);
+        // filter out older readings of same stationId
+        Map<Integer, Measurement> uniqueLatestMeasurements = new LinkedHashMap<>();
+        for (Measurement measurement : latestMeasurements) {
+            int id = measurement.getId();
+            // Check if this id is already in the map
+            if (!uniqueLatestMeasurements.containsKey(id)) {
+                uniqueLatestMeasurements.put(id, measurement);
             }
-
-            ResponseEntity<String> response = restTemplate.getForEntity(BASE_URL, String.class);
-            String responseBody = response.getBody();
-
-            ObjectMapper mapper = new ObjectMapper();
-            mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-            TypeReference<List<Measurement>> typeReference = new TypeReference<List<Measurement>>() {};
-            List<Measurement> measurements = mapper.readValue(responseBody, typeReference);
-
-            return measurements;
-        } catch (Exception e) {
-            LOG.error(e.getMessage());
-            throw new RuntimeException();
         }
+        List<Measurement> temp = new ArrayList<>(uniqueLatestMeasurements.values());
+        return temp;
+    }
+
+    private String meetJeStadDateTimeConverter(Instant instant) throws ParseException {
+        String shortDateTime = instant
+                .toString()
+                .split("\\.")[0];
+
+        SimpleDateFormat instantFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+        SimpleDateFormat meetJeStadFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+
+        Date date = instantFormat.parse(shortDateTime);
+        return meetJeStadFormat.format(date);
+    }
+
+    private List<Measurement> getMeetJeStadMeasurements(String startDateTime, String endDateTime) throws JsonProcessingException {
+        BASE_URL += "&start=" + startDateTime + "&end=" + endDateTime;
+        ResponseEntity<String> response = restTemplate.getForEntity(BASE_URL, String.class);
+        String responseBody = response.getBody();
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        TypeReference<List<Measurement>> typeReference = new TypeReference<List<Measurement>>() {
+        };
+        return mapper.readValue(responseBody, typeReference);
     }
 }
