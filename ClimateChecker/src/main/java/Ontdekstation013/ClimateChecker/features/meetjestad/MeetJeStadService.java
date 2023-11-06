545ca19e97131aba9e9b48f22e5619f98d87dd19
Niels Eppenhof <n.eppenhof@gmail.com>
2023-11-06 14:59:58 +0100

    Formatting

--- ClimateChecker/src/main/java/Ontdekstation013/ClimateChecker/features/meetjestad/MeetJeStadService.java
+++ ClimateChecker/src/main/java/Ontdekstation013/ClimateChecker/features/meetjestad/MeetJeStadService.java
@@ -27,10 +27,10 @@ public class MeetJeStadService {
     private String baseUrl = "https://meetjestad.net/data/?type=sensors&format=json";
     private int minuteLimit = 35;
     private float[][] cityLimits = {
-            { 51.65156373065635f, 5.217787919413907f },
-            { 51.51818572766462f, 5.227145728754213f },
-            { 51.52666590649518f, 4.911805911309284f },
-            { 51.65077670571181f, 4.957086656750303f }
+        { 51.65156373065635f, 5.217787919413907f },
+        { 51.51818572766462f, 5.227145728754213f },
+        { 51.52666590649518f, 4.911805911309284f },
+        { 51.65077670571181f, 4.957086656750303f }
     };
     public List<Measurement> getMeasurements(MeetJeStadParameters params)
     {
