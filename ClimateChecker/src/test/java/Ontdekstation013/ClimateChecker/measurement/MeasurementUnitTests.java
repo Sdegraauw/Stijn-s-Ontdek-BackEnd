564095b1c7fa863a9b0f5abd1e97d1d7cb4535ae
Niels Eppenhof <n.eppenhof@gmail.com>
2023-11-13 09:22:10 +0100

    Fixed merge conflict

--- ClimateChecker/src/test/java/Ontdekstation013/ClimateChecker/measurement/MeasurementUnitTests.java
+++ ClimateChecker/src/test/java/Ontdekstation013/ClimateChecker/measurement/MeasurementUnitTests.java
@@ -1,67 +1,57 @@
 package Ontdekstation013.ClimateChecker.measurement;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import Ontdekstation013.ClimateChecker.features.meetjestad.MeetJeStadService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 
-import Ontdekstation013.ClimateChecker.features.measurement.MeasurementController;
-import Ontdekstation013.ClimateChecker.features.measurement.MeasurementDTO;
+import Ontdekstation013.ClimateChecker.features.measurement.endpoint.MeasurementDTO;
 import Ontdekstation013.ClimateChecker.features.measurement.MeasurementService;
 
 @ExtendWith(MockitoExtension.class)
 public class MeasurementUnitTests {
-    
     @InjectMocks
-    private MeasurementController measurementController;
+    private MeasurementService measurementService;
 
     @Mock
-    private MeasurementService measurementService;
+    private MeetJeStadService meetJeStadService;
+    private List<MeasurementDTO> measurementList;
 
     private Logger LOG = LogManager.getLogger(MeasurementUnitTests.class);
 
-    @Test
-    public void testGetLatestMeasurementsUnitTest() {
+    @BeforeEach
+    public void init() {
+        measurementList = new ArrayList<>();
 
-        MeasurementDTO measurementDTO = MeasurementDTO.builder()
-        .id(1)
-        .timestamp("2020-01-01 00:00:00")
-        .longitude(35)
-        .latitude(70)
-        .temperature(20)
-        .build();
-
-        List<MeasurementDTO> measurementDTOList = new ArrayList<>();
-        measurementDTOList.add(measurementDTO);
-
-        when(measurementService.getLatestMeasurements()).thenReturn(measurementDTOList);
-
-        ResponseEntity<List<MeasurementDTO>> response = measurementController.getLatestMeasurements();
-        
-        LOG.info(response.getBody().get(0).toString());
-
-        assertEquals(HttpStatus.OK, response.getStatusCode());
-        assertNotNull(response.getBody());
-        assertEquals(measurementDTOList, response.getBody());
-        assertEquals(measurementDTOList.get(0).getId(), response.getBody().get(0).getId());
-        assertEquals(measurementDTOList.get(0).getTimestamp(), response.getBody().get(0).getTimestamp());
-        assertEquals(measurementDTOList.get(0).getLongitude(), response.getBody().get(0).getLongitude());
-        assertEquals(measurementDTOList.get(0).getLatitude(), response.getBody().get(0).getLatitude());
-        assertEquals(measurementDTOList.get(0).getTemperature(), response.getBody().get(0).getTemperature());
-
-        
+        measurementList.add(new MeasurementDTO(1, "07-10-2023 15:00", 51.578000f, 4.992980f, 22.0f, 50.0f));
+        measurementList.add(new MeasurementDTO(2, "07-10-2023 14:00", 51.578000f, 4.992980f, 21.0f, 50.0f));
+        measurementList.add(new MeasurementDTO(3, "07-10-2023 16:00", 51.578000f, 4.992980f, 19.0f, 50.0f));
+        measurementList.add(new MeasurementDTO(3, "07-10-2023 15:00", 51.578000f, 4.992980f, 19.0f, 50.0f));
+        measurementList.add(new MeasurementDTO(4, "07-10-2023 16:00", 51.578000f, 4.992980f, 19.5f, 50.0f));
+        measurementList.add(new MeasurementDTO(5, "07-10-2023 16:00", 51.578000f, 4.992980f, 19.0f, 50.0f));
+        measurementList.add(new MeasurementDTO(5, "07-10-2023 17:00", 51.578000f, 4.992980f, 19.5f, 50.0f));
     }
 
+    @Test
+    public void getLatestMeasurements() {
+        // Arrange
+        //when(measurementService.getLatestMeasurements()).thenReturn(measurementList);
+
+        // Act
+        //List<MeasurementDTO> dtos = measurementService.getLatestMeasurements();
+
+        // Assert
+        //assertEquals(dtos.size(), 5);
+    }
 }
