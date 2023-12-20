7a07a5fda6f4e449ffdd42ebbd80edd96fd75ae4
Niels Eppenhof <n.eppenhof@gmail.com>
2023-12-20 15:24:02 +0100

    It now very quick is yes

--- ClimateChecker/src/main/java/Ontdekstation013/ClimateChecker/features/measurement/Measurement.java
+++ ClimateChecker/src/main/java/Ontdekstation013/ClimateChecker/features/measurement/Measurement.java
@@ -14,6 +14,7 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+@NamedEntityGraph(name = "Measurement.measurementResults", attributeNodes = { @NamedAttributeNode("measurementResults") })
 public class Measurement {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- ClimateChecker/src/main/java/Ontdekstation013/ClimateChecker/features/measurement/MeasurementRepository.java
+++ ClimateChecker/src/main/java/Ontdekstation013/ClimateChecker/features/measurement/MeasurementRepository.java
@@ -1,6 +1,7 @@
 package Ontdekstation013.ClimateChecker.features.measurement;
 
 import Ontdekstation013.ClimateChecker.features.station.Station;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,16 +13,27 @@ public interface MeasurementRepository extends JpaRepository<Measurement, Long> 
     Measurement findTopByOrderByIdDesc();
 
     // Get all measurements between timeframe
+    @EntityGraph(type=EntityGraph.EntityGraphType.FETCH,
+            value = "Measurement.measurementResults")
     List<Measurement> findAllByMeasurementTimeBeforeAndMeasurementTimeAfterOrderByMeasurementTimeDesc(Instant endDate, Instant startDate);
 
     // Get all unique measurements between timeframe
+    @EntityGraph(type=EntityGraph.EntityGraphType.FETCH,
+            value = "Measurement.measurementResults")
     List<Measurement> findDistinctByMeasurementTimeBeforeAndMeasurementTimeAfterOrderByMeasurementTimeDesc(Instant endDate, Instant startDate);
 
     // Get measurements by station between timeframe
+    @EntityGraph(type=EntityGraph.EntityGraphType.FETCH,
+            value = "Measurement.measurementResults")
     List<Measurement> findByStationAndMeasurementTimeBeforeAndMeasurementTimeAfter(Station station, Instant endDate, Instant startDate);
 
+    // Get measurements by list of stations and measurement type between timeframe
+    @EntityGraph(type=EntityGraph.EntityGraphType.FETCH,
+            value = "Measurement.measurementResults")
     List<Measurement> findAllByStationInAndMeasurementResults_MeasurementTypeAndMeasurementTimeBeforeAndMeasurementTimeAfter(List<Station> stations, MeasurementType type, Instant endDate, Instant startDate);
 
     // Get measurements by station and measurement type between timeframe
+    @EntityGraph(type=EntityGraph.EntityGraphType.FETCH,
+            value = "Measurement.measurementResults")
     List<Measurement> findByStationAndMeasurementResults_MeasurementTypeAndMeasurementTimeBeforeAndMeasurementTimeAfter(Station station, MeasurementType type, Instant endDate, Instant startDate);
 }
