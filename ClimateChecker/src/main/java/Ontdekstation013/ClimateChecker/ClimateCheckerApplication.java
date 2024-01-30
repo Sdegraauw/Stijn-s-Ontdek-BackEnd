e8e2203316f65f3283a58c2e15f1dac34a239b9c
Sdegraauw <144223601+Sdegraauw@users.noreply.github.com>
2024-01-30 15:43:16 +0100

    DELETE

--- ClimateChecker/src/main/java/Ontdekstation013/ClimateChecker/ClimateCheckerApplication.java
+++ ClimateChecker/src/main/java/Ontdekstation013/ClimateChecker/ClimateCheckerApplication.java
@@ -5,11 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ClimateCheckerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ClimateCheckerApplication.class, args);
 	}
-
-
 }
 
